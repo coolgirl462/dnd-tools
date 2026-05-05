cefd55dc89426e468ac33b8a8e21682458221231
krissyx3 <chrisdynish1@gmail.com>
2026-05-05 16:56:32 +0530

    fixed imports

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
-from ui_main import Ui_MainWindow
 
 class MainUi(QtWidgets.QWidget):
 	def __init__(self):
